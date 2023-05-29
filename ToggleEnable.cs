d8020ec9087d27a78e17863ac87a9f291c0181b4
yakimannzyuu <yakimannzyuu.vrc@gmail.com>
2023-05-29 16:36:32 +0900

    enable shortcut tool refactring

--- ToggleEnable.cs
+++ ToggleEnable.cs
@@ -5,55 +5,52 @@ namespace Yakimannzyuu
 {
     public class ToggleEnable : Editor
     {
-        const string MenuNameChangeEnable = "Tools/Yakimannzyuu/ChangeEnable &a";
-        const string MenuNameSetEditorOnly = "Tools/Yakimannzyuu/ChangeEditorOnly &e";
-        const string Untagged = "Untagged";
-        const string EditorOnly = "EditorOnly";
+        const string _MenuNameChangeEnable = "Tools/Yakimannzyuu/ChangeEnable #a";
+        const string _MenuNameSetEditorOnly = "Tools/Yakimannzyuu/ChangeEditorOnly #e";
+        const string _Untagged = "Untagged";
+        const string _EditorOnly = "EditorOnly";
         // アクティブ/非アクティブ化のショートカットを追加します。
-        // Shift + A:非アクティブが含まれている場合はすべてアクティブに
-        // Shift + A:すべてアクティブならすべて非アクティブに。
-        [MenuItem(MenuNameChangeEnable)]
+        // Shift + a : 非アクティブが含まれている場合はすべてアクティブに
+        // Shift + a : すべてアクティブならすべて非アクティブに。
+        [MenuItem(_MenuNameChangeEnable)]
         static void ChangeEnable()
         {
-            bool toActive = false;
             GameObject[] selectionObj = Selection.gameObjects;
-            foreach(GameObject obj in selectionObj)
-            {
-                if(!obj.activeSelf)
-                {
-                    toActive = true;
-                    break;
-                }
-            }
-            Undo.RecordObjects(selectionObj, "ChangeEnable");
-            foreach(GameObject obj in selectionObj)
-            {
-                obj.SetActive(toActive);
-            }
-            Undo.RecordObjects(selectionObj, "ChangeEnable");
+
+            Undo.RecordObjects(selectionObj, _MenuNameChangeEnable);
+            SetActives(selectionObj, !FindUnActive(selectionObj));
+            Undo.RecordObjects(selectionObj, $"{_MenuNameChangeEnable} end");
         }
 
-        // EditorOnlyを切り替える
-        [MenuItem(MenuNameSetEditorOnly)]
+        // Shift + e : EditorOnlyを切り替える
+        [MenuItem(_MenuNameSetEditorOnly)]
         static void SetEditorOnly()
         {
-            bool toActive = false;
             GameObject[] selectionObj = Selection.gameObjects;
+            bool toActive = !FindUnActive(selectionObj);
+
+            Undo.RecordObjects(selectionObj, _MenuNameSetEditorOnly);
+            SetActives(selectionObj, toActive);
             foreach(GameObject obj in selectionObj)
-            {
+                obj.tag = toActive ? _Untagged : _EditorOnly;
+            Undo.RecordObjects(selectionObj, $"{_MenuNameSetEditorOnly} end");
+        }
+
+        // アクティブかどうかを取得します。falseが優先されます。
+        public static bool FindUnActive(GameObject[] objects)
+        {
+            foreach(GameObject obj in objects)
                 if(!obj.activeSelf)
-                {
-                    toActive = true;
-                    break;
-                }
-            }
-            Undo.RecordObjects(selectionObj, "ChangeTagEditorOnly");
-            foreach(GameObject obj in selectionObj)
-            {
-                obj.SetActive(toActive);
-                obj.tag = toActive ? Untagged : EditorOnly;
-            }
-            Undo.RecordObjects(selectionObj, "ChangeTagEditorOnly");
+                    return false;
+            return true;
+        }
+
+        // オブジェクトのアクティブを設定します。
+        public static void SetActives(GameObject[] objects, bool active)
+        {
+            foreach(GameObject obj in objects)
+                if(obj != null)
+                    obj.SetActive(active);
         }
     }
 }
